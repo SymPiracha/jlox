508412fdfa84344c35e8f2079b62c705e7bd5fe8
SymPiracha <sym.piracha@gmail.com>
2024-09-29 08:52:52 -0400

    wire it up

--- src/lox/Parser.java
+++ src/lox/Parser.java
@@ -3,13 +3,32 @@ package lox;
 import java.util.List;
 import static lox.TokenType.*;
 
+/**
+ * expression    → equality ;
+ * equality      → comparison ( ( "!=" | "==" ) comparison )* ;
+ * comparison    → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+ * term          → factor ( ( "-" | "+" ) factor )* ;
+ * factor        → unary ( ( "/" | "*" ) unary )* ;
+ * unary         → ( "!" | "-" ) unary | primary ;
+ *  primary       → "false" | "true" | "nil" | NUMBER | STRING | "(" expression ")" ;
+ */
+
 class Parser {
+    private static class ParseError extends RuntimeException {}
     private final List<Token> tokens;
     private int current = 0;
 
     Parser(List<Token> tokens) {
         this.tokens = tokens;
     }
+
+    Expr parse() {
+        try {
+          return expression();
+        } catch (ParseError error) {
+          return null;
+        }
+      }
 
     private Expr expression() {
         return equality();
@@ -72,16 +91,18 @@ class Parser {
         if (match(FALSE)) return new Expr.Literal(false);
         if (match(TRUE)) return new Expr.Literal(true);
         if (match(NIL)) return new Expr.Literal(null);
-
+    
         if (match(NUMBER, STRING)) {
-            return new Expr.Literal(previous().literal);
+          return new Expr.Literal(previous().literal);
+        }
+    
+        if (match(LEFT_PAREN)) {
+          Expr expr = expression();
+          consume(RIGHT_PAREN, "Expect ')' after expression.");
+          return new Expr.Grouping(expr);
         }
 
-        if (match(LEFT_PAREN)) {
-            Expr expr = expression();
-            // consume(RIGHT_PAREN, "Expect ')' after expression.");
-            return new Expr.Grouping(expr);
-        }
+        throw error(peek(), "Expect expression.");
     }
 
     private boolean match(TokenType ...tokens) {
@@ -93,6 +114,38 @@ class Parser {
         }
         return false;
     }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type)) return advance();
+        throw error(peek(), message);
+    }
+
+    private ParseError error(Token token, String message) {
+        Lox.error(token, message);
+        return new ParseError();
+    }
+
+    private void synchronize() {
+        advance();
+    
+        while (!isAtEnd()) {
+          if (previous().type == SEMICOLON) return;
+    
+          switch (peek().type) {
+            case CLASS:
+            case FUN:
+            case VAR:
+            case FOR:
+            case IF:
+            case WHILE:
+            case PRINT:
+            case RETURN:
+              return;
+          }
+    
+          advance();
+        }
+      }
 
     private boolean check(TokenType type) {
         if (isAtEnd()) return false;
